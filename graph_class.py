1bc4a831a28e7e46786ebf50dee780afb651d7ba
m.wilcox27@ncf.edu <m.wilcox27@ncf.edu>
2023-11-19 16:43:26 -0500

    Add Max_people()

--- graph_class.py
+++ graph_class.py
@@ -27,8 +27,6 @@ class flight_graph:
                 self.vert_dict = {vert:self.vertcount}
                 self.graph = np.zeros((1,1), dtype = int)
                 self.vertcount += 1
-                print("error")
-                print(self.vert_dict)
 
             else:
                 self.vert_dict[vert] = self.vertcount
@@ -42,14 +40,11 @@ class flight_graph:
 
 #Adding edge. the verts are the datasets verts. 
     def addEdge(self,u_vert,v_vert,weight):
-        print(self.vert_dict)
 
         if u_vert not in self.vert_dict:
-            print("no_u")
             self.addVert(u_vert)
         if v_vert not in self.vert_dict:
             self.addVert(v_vert)
-            print("no_v")
         self.graph[self.vert_dict[u_vert], self.vert_dict[v_vert]] = self.graph[self.vert_dict[u_vert], self.vert_dict[v_vert]] + weight
 
 # Adding data to the graph based upon a pandas dataframe uCol, vCol, weightCol should be the name of the column in the df
@@ -84,6 +79,19 @@ class flight_graph:
                     linked_list += [[row, col, self.graph[row,col]]]
         return np.array(linked_list)
 
+    def adjacency_list(self):
+        adjac_list = []
+        for row in self.graph:
+            row_list = []
+            print(row)
+            col_count = 0
+            for col in row:
+                if col > 0:
+                    row_list += [col_count]
+                col_count += 1
+            adjac_list += [row_list]
+        return adjac_list
+
 # Limit to a number of layovers produces a new graph
 # Save as a ne variable to retain original graph
 ### Error Incountered. Queing would go through and add every edge and their would be double outputing the wrong graph.
@@ -91,22 +99,21 @@ class flight_graph:
 ### Fixing list - error in addEdge - making a new dictionary
 ### This was due to the edge was imputing the value not the key of the edge. 
 ### Need to put the key value in but that is going to increase runtime significantly.
-    def limitLayovers(self, startAirport, maxLayover = 1):
+
+# Limit Layover mimics Breadth First Search but declares an error at the number of layovers
+# limitLayover runs in O(M*E) where max layovers is the number of layovers we state, in this case we limit it to 1 or 0 which means it runs through a constnat amoutn of times so it shoud be O(E)
+    def limitLayovers(self, startAirport, maxLayover = 1): # THIS FUNCTION HALUCINATES IF maxLayover > 1. DO NOT HAVE A MAX LAYOVER GREATER THAN 1
         start_vert = self.vert_dict[startAirport]
         edge_list = self.linkedList()
         layoverGraph = flight_graph()
         layoverGraph.manual_inport_vert_dict(self.vert_dict) 
         layoverGraph.manual_import_graph(np.zeros((len(self.vert_dict.keys()),len(self.vert_dict.keys())), dtype = int))
         layoverGraph.displayGraph()
-        print(layoverGraph.manual_retrieve_dict())
         check_queue = [start_vert]
         shell_count = 0
-        # Import Key and val into a list to decrease the runtime and retrive the key that the edge is assocaited. to. 
+        # Import Key and val into a list to decrease the runtime and retrive the key that the edge is assocaited. to. This run in O(1) most efficient way to do this. 
         key_list = list(layoverGraph.manual_retrieve_dict().keys())
         val_list = list(layoverGraph.manual_retrieve_dict().values())
-        print("key",key_list)
-        print("val",val_list)
-
         while shell_count <= maxLayover:   
             queue_pulled = check_queue
             # Clear Queue
@@ -114,70 +121,96 @@ class flight_graph:
             for vert in queue_pulled:
                 for edge in edge_list:
                     if edge[0] == vert:
-                        print(edge)
                         layoverGraph.manual_retrieve_dict
                         layoverGraph.addEdge(key_list[val_list.index(edge[0])], key_list[val_list.index(edge[1])], edge[2])
                         check_queue += [edge[1]]
-            print("*")
-            print(layoverGraph.displayGraph())
-            print("*")
+          
             shell_count += 1
             # print(check_queue)
             # print('-'*20)
         return layoverGraph
 
 
+    def initialize_max(self, startVert):
+        import math
+        verts_current_capacity = list(np.zeros(len(self.graph), int))
+        verts_current_capacity[self.vert_dict[startVert]] = math.inf
+        return verts_current_capacity
+        
+    def find_max(self, input_startVert, input_endVert, layover = True):
+        import math
+        startVert = self.vert_dict[input_startVert]
+        endVert = self.vert_dict[input_endVert]
+        vert_capacity = self.initialize_max(input_startVert)
+        adj_list = self.adjacency_list()
+        first_shell = adj_list[startVert]
+        for v_vert in first_shell:
+            vert_capacity[v_vert] = self.graph[startVert,v_vert]
+            if layover == False:
+                max_people = vert_capacity[endVert]
+                return max_people
+            else:
+                if endVert in adj_list[v_vert]:
+                    if vert_capacity[v_vert] >=  self.graph[v_vert,endVert]:
+                        vert_capacity[endVert] += self.graph[v_vert,endVert]
+                    else:
+                        vert_capacity[endVert] += vert_capacity[v_vert]
+        max_people = vert_capacity[endVert]
+        return max_people
+               
 
+df = pd.DataFrame(np.array([[1,2,10],[1,3,5],[2,4,3],[3,4,1], [2,3,1]]), columns=['A', 'B', 'C'])
 
-df = pd.DataFrame(np.array([[3,2,1],[2,3,2],[1,3,5]]), columns=['A', 'B', 'C'])
-
-print(df)
+# print(df)
 
 
 x = flight_graph()
 
 x.importGraphData(df, 'A', 'B', 'C')
-
 x.displayGraph()
-print("-------")
-x2 = x.limitLayovers(3,1)
-print("-------")
-print("2")
-print(x.linkedList())
-print("-------")
-x2.displayGraph()
-print("-------")
-print(x2.manual_retrieve_dict())
-# x.linkedList()
+# print(x.adjacency_list())
+# x.initialize_max(1)
+print(x.find_max(1,4))
+# x.displayGraph()
+# print("-------")
+# x2 = x.limitLayovers(3,1)
+# print("-------")
+# print("2")
 # print(x.linkedList())
+# print("-------")
+# x2.displayGraph()
+# print("-------")
+# print(x2.manual_retrieve_dict())
+# # x.linkedList()
+# # print(x.linkedList())
 
 
-num_vertices = 50
-vertices = [f'V{i}' for i in range(1, num_vertices + 1)]
-num_edges = 25  # Number of edges in the graph
+# num_vertices = 50
+# vertices = [f'V{i}' for i in range(1, num_vertices + 1)]
+# num_edges = 25  # Number of edges in the graph
 
-# Creating random edges and their weights
-np.random.seed(42)
-edges = []
-weights = np.random.randint(1, 20, size=num_edges)
-for _ in range(num_edges):
-    edge = np.random.choice(vertices, size=2, replace=False)
-    edge_weight = np.random.choice(weights)
-    edges.append([edge[0], edge[1], edge_weight])
+# # Creating random edges and their weights
+# np.random.seed(42)
+# edges = []
+# weights = np.random.randint(1, 20, size=num_edges)
+# for _ in range(num_edges):
+#     edge = np.random.choice(vertices, size=2, replace=False)
+#     edge_weight = np.random.choice(weights)
+#     edges.append([edge[0], edge[1], edge_weight])
 
-# Creating the dataframe
-df = pd.DataFrame(edges, columns=['First Vertice', 'Second Vertice', 'Weight'])
-print(df)
+# # Creating the dataframe
+# df = pd.DataFrame(edges, columns=['First Vertice', 'Second Vertice', 'Weight'])
+# print(df)
 
-y = flight_graph()
-y.importGraphData(df, "First Vertice", "Second Vertice" , "Weight")
-y.displayGraph()
+# y = flight_graph()
+# y.importGraphData(df, "First Vertice", "Second Vertice" , "Weight")
+# y.displayGraph()
 
-z = y.limitLayovers("V50",0)
-y.displayGraph()
-z.displayGraph()
-print(z.linkedList())
+# z = y.limitLayovers("V50",0)
+# y.displayGraph()
+# z.displayGraph()
+# print(z.linkedList())
 
 
 
-# New error, if you have 3 layovers, it just adds the throughpukt of the same location, I dont believe that is what we want. Wont Impact us on this project as we are only looking at max 1 layover but is something to consieder
+# # New error, if you have 3 layovers, it just adds the throughpukt of the same location, I dont believe that is what we want. Wont Impact us on this project as we are only looking at max 1 layover but is something to consieder
