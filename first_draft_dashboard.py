6408521f770ac6ffae1a91d2ce451b67ae9ea3ce
Sabrina Harris <142846771+sabrinaharris389@users.noreply.github.com>
2023-12-09 15:35:23 -0500

    sort alphabetically, set default inputs

--- first_draft_dashboard.py
+++ first_draft_dashboard.py
@@ -8,12 +8,12 @@ airport_data = pd.read_csv('c:/Users/rocky/iCloudDrive/Desktop/NCF Fall 2023/Alg
 
 root = Tk()
 root.title('Flights') #title of window
-#root.iconbitmap('C:/Users/rocky/iCloudDrive/Desktop/NCF Fall 2023/Algorithms/Group projects/Group project 2/Final Stuff/Plane.ico') #cute icon
+root.iconbitmap('C:/Users/rocky/iCloudDrive/Desktop/NCF Fall 2023/Algorithms/Group projects/Group project 2/Final Stuff/Plane.ico') #cute icon
 root.geometry('900x600') #size of window
 
 #create lists with unique departing airports and destination airports
-departing_airports = airport_data['source_airport_name'].unique()
-destination_airports = airport_data['destination_airport_name'].unique()
+departing_airports = airport_data['source_airport_name'].sort_values().unique()
+destination_airports = airport_data['destination_airport_name'].sort_values().unique()
 
 
 #this function will be used with the button. 
@@ -47,15 +47,17 @@ label1 = Label(root, text = 'Select Departing Airport').grid(row = 0, column = 0
 label2 = Label(root, text = 'Select Destination Airport').grid(row = 0, column = 1)
 
 #define dropdown boxes
+selected_departing_airport.set('Orlando') #set default option
 dropdownboxDeparting = OptionMenu(root, selected_departing_airport,*departing_airports) #must have the star to show the outside variable
-dropdownboxDeparting.grid(row = 1, column = 0)
+dropdownboxDeparting.grid(row = 1, column = 0, padx = 25)
 
+selected_destination_airport.set('San Diego') #set default option
 dropdownboxDestination = OptionMenu(root, selected_destination_airport,*destination_airports) #must have the star to show the outside variable
-dropdownboxDestination.grid(row = 1, column = 1)
+dropdownboxDestination.grid(row = 1, column = 1, padx = 25)
 
 #define enter button
 enter_button = Button(root, text = 'click to enter selection', command = show_airport_ids )
-enter_button.grid(row = 2, column = 0)
+enter_button.grid(row = 2, column = 0, padx = 50)
 
 
 
