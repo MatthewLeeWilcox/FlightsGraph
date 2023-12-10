e9d144b45deb879d4da1827c5c6a04a2055366fd
Sabrina Harris <142846771+sabrinaharris389@users.noreply.github.com>
2023-12-10 16:30:51 -0500

    class structure update
    updated into a class structure for future incorporation with graph_class.py

--- first_draft_dashboard.py
+++ first_draft_dashboard.py
@@ -3,62 +3,69 @@ from PIL import ImageTk, Image
 import pandas as pd 
 
 #load data
-airport_data = pd.read_csv('c:/Users/rocky/iCloudDrive/Desktop/NCF Fall 2023/Algorithms/Group projects/Group project 2/Data/routes_w_capacities_starter_set.csv')
-
+df_routes = pd.read_csv('c:/Users/rocky/iCloudDrive/Desktop/NCF Fall 2023/Algorithms/Group projects/Group project 2/Data/routes_w_capacities_starter_set.csv')
 
 root = Tk()
 root.title('Flights') #title of window
 root.iconbitmap('C:/Users/rocky/iCloudDrive/Desktop/NCF Fall 2023/Algorithms/Group projects/Group project 2/Final Stuff/Plane.ico') #cute icon
 root.geometry('900x600') #size of window
 
-#create lists with unique departing airports and destination airports
-departing_airports = airport_data['source_airport_name'].sort_values().unique()
-destination_airports = airport_data['destination_airport_name'].sort_values().unique()
+class flight_graph:
+    
+    def __init__(self,main_root):
+        #create lists with unique departing airports and destination airports
+        self.departing_airports = df_routes['source_airport_name'].sort_values().unique()
+        self.destination_airports = df_routes['destination_airport_name'].sort_values().unique()
+
+        #storing the selected values
+        self.selected_departing_airport = StringVar()
+        self.selected_destination_airport = StringVar()
+
+        #set labels
+        self.label1 = Label(main_root, text = 'Select Departing Airport').grid(row = 0, column = 0)
+        self.label2 = Label(main_root, text = 'Select Destination Airport').grid(row = 0, column = 1)
+
+        #define dropdown boxes
+        self.selected_departing_airport.set('Orlando') #set default option
+        self.dropdownboxDeparting = OptionMenu(main_root, self.selected_departing_airport,*self.departing_airports) #must have the star to show the outside variable
+        self.dropdownboxDeparting.grid(row = 1, column = 0, padx = 25)
+
+        self.selected_destination_airport.set('San Diego') #set default option
+        self.dropdownboxDestination = OptionMenu(main_root, self.selected_destination_airport,*self.destination_airports) #must have the star to show the outside variable
+        self.dropdownboxDestination.grid(row = 1, column = 1, padx = 25)
+
+        #define enter button
+        self.enter_button = Button(main_root, text = 'click to enter selection', command = self.find_airport_ids )
+        self.enter_button.grid(row = 2, column = 0, padx = 50)
+
+        #create frames to put output
+        self.output_frame_departing = Frame(main_root)
+        self.output_frame_departing.grid(row = 3, column = 0)
+
+        self.output_frame_destination = Frame(main_root)
+        self.output_frame_destination.grid(row = 4, column = 0)
 
 
-#this function will be used with the button. 
-def show_airport_ids():
-    #get departing airport info
-    departing = selected_departing_airport.get()
-    dep_filter = airport_data['source_airport_name']  == departing
-    dep = airport_data[dep_filter]
-    dep_id = dep['source_airport_ID'].unique()
-    dep_label = Label(root, text = 'departing airport id; ' + str(dep_id))
+    def find_airport_ids(self):
+        #get departing airport info
+        self.departing = self.selected_departing_airport.get()
+        self.dep_filter = df_routes['source_airport_name']  == self.departing
+        self.dep = df_routes[self.dep_filter]
+        self.dep_id = self.dep['source_airport_ID'].unique()
+        self.dep_label = Label(self.output_frame_departing, text = 'departing airport id; ' + str(self.dep_id))
 
-    #get destination airport info
-    destination = selected_destination_airport.get()
-    dest_filter = airport_data['destination_airport_name']==destination
-    dest = airport_data[dest_filter]
-    dest_id = dest['destination_airport_ID'].unique()
-    dest_label = Label(root, text = 'destination airport id; ' + str(dest_id))
+        #get destination airport info
+        self.destination = self.selected_destination_airport.get()
+        self.dest_filter =df_routes['destination_airport_name']==self.destination
+        self.dest = df_routes[self.dest_filter]
+        self.dest_id = self.dest['destination_airport_ID'].unique()
+        self.dest_label = Label(self.output_frame_destination, text = 'destination airport id; ' + str(self.dest_id))
 
-    #put labels on screen
-    dep_label.grid(row = 3, column = 0)
-    dest_label.grid(row = 4, column = 0)
-
+        #put labels on screen
+        self.dep_label.grid(row = 3, column = 0)
+        self.dest_label.grid(row = 4, column = 0)
 
 
-#storing the selected values
-selected_departing_airport = StringVar()
-selected_destination_airport = StringVar()
-
-#set labels
-label1 = Label(root, text = 'Select Departing Airport').grid(row = 0, column = 0)
-label2 = Label(root, text = 'Select Destination Airport').grid(row = 0, column = 1)
-
-#define dropdown boxes
-selected_departing_airport.set('Orlando') #set default option
-dropdownboxDeparting = OptionMenu(root, selected_departing_airport,*departing_airports) #must have the star to show the outside variable
-dropdownboxDeparting.grid(row = 1, column = 0, padx = 25)
-
-selected_destination_airport.set('San Diego') #set default option
-dropdownboxDestination = OptionMenu(root, selected_destination_airport,*destination_airports) #must have the star to show the outside variable
-dropdownboxDestination.grid(row = 1, column = 1, padx = 25)
-
-#define enter button
-enter_button = Button(root, text = 'click to enter selection', command = show_airport_ids )
-enter_button.grid(row = 2, column = 0, padx = 50)
-
-
+trial = flight_graph(root)
 
 root.mainloop()
